15301d33e739e1b38e00d5adb561933b873c4f12
Josh Mu <hello@joshmu.dev>
2021-02-06 16:13:55 +1000

    asset management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,36 @@
+// @ts-nocheck
 import _ from 'lodash'
+import './style.scss'
+import Logo from './logo.jpg'
+import Data from './components/my-component/data.xml'
+import Notes from './components/my-component/data.csv'
+import toml from './components/my-component/data.toml';
+import yaml from './components/my-component/data.yaml';
+import json from './components/my-component/data.json5';
+
+console.log(toml.title); // output `TOML Example`
+console.log(toml.owner.name); // output `Tom Preston-Werner`
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
+
+console.log(json.title); // output `JSON5 Example`
+console.log(json.owner.name); // output `Tom Preston-Werner`
 
 const component = () => {
   const elem = document.createElement('div')
 
   // lodash is imported
   elem.innerHTML = _.join(['Hello', 'webpack'], ' ')
+  elem.classList.add('hello')
+
+  // add image to existing div
+  const myLogo = new Image()
+  myLogo.src = Logo
+  elem.appendChild(myLogo)
+
+  console.log(Data)
+  console.log(Notes)
 
   return elem
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,73 @@
 const path = require('path')
+const toml = require('toml')
+const yaml = require('yamljs')
+const json5 = require('json5')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      // SCSS
+      {
+        test: /\.s[ac]ss$/i,
+        // * reverse order chaining
+        use: [
+          // 4. create 'style' nodes from JS strings
+          'style-loader',
+          // 3. translate css to commonjs
+           'css-loader',
+           // 2. fix url()
+           'resolve-url-loader',
+           // 1. compile sass > css
+            'sass-loader']
+      },
+      // IMAGES
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      // FONTS
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+      // CSV
+      {
+        test: /.(csv|tsv)$/i ,
+        use: ['csv-loader']
+      },
+      // XML
+      {
+        test: /.xml$/i ,
+        use: ['xml-loader']
+      },
+      // TOML
+      {
+        test: /.toml$/i,
+        type: 'json',
+        parser: {
+          parse: toml.parse
+        }
+      },
+      // YAML
+      {
+        test: /.yaml$/i,
+        type: 'json',
+        parser: {
+          parse: yaml.parse
+        }
+      },
+      {
+        test: /.json5$/i,
+        type: 'json',
+        parser: {
+          parse: json5.parse
+        }
+      },
+    ]
   }
 }
